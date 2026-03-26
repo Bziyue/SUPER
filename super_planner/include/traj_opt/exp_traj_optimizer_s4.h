997abe53a75cba43935959f244f2fd4778760f31
Bziyue <954164207@qq.com>
2026-03-26 11:06:18 +0800

    sync latest spline optimizer

--- super_planner/include/traj_opt/exp_traj_optimizer_s4.h
+++ super_planner/include/traj_opt/exp_traj_optimizer_s4.h
@@ -70,7 +70,7 @@ namespace traj_opt {
         std::ofstream penalty_log;
         ros_interface::RosInterface::Ptr ros_ptr_;
         Optimizer optimizer_;
-        Optimizer::Workspace spline_workspace_;
+        Optimizer::OptimizationContext spline_context_;
         SpatialMap spatial_map_;
         spline_opt::LinearTimeCost time_cost_;
         spline_opt::ExpPenaltyIntegralCost integral_cost_;
